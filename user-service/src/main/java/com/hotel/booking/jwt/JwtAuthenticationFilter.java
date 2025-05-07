900810d8e189e2e5542d20f2485670f6ce221e14
Mauricio <marinmauricio1000@gmail.com>
2025-05-07 14:09:04 -0500

    Spring security set up in loggin-in part 1

--- user-service/src/main/java/com/hotel/booking/jwt/JwtAuthenticationFilter.java
+++ user-service/src/main/java/com/hotel/booking/jwt/JwtAuthenticationFilter.java
@@ -4,7 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -21,14 +29,69 @@ import java.io.IOException;
  * <h2>Anotación @Component</h2>
  */
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private static JwtService jwtService;
+    private static UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        filterChain.doFilter(request, response);
+        String token = this.getTokenFromRequest(request);
 
+        if(token == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String username = jwtService.getUsernameFromToken(token);
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if(jwtService.isTokenValid(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authUser =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails, null, userDetails.getAuthorities()
+                        );
+
+                //No entiendo muy bien por qué se agrega esto jeje
+                authUser.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authUser);
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * <p>
+     *     Verificamos si existen la sección de AUTHORIZATION
+     *     en el encabezado de la petición y debemos mirar que empiece con la palabra Bearer
+     * </p>
+     *
+     * <b>Ejemplo de un Header de HTTP</b>
+     *<blockquote><pre>
+     * GET /api/productos HTTP/1.1
+     * Host: api.tu-ecommerce.com
+     * Authorization: Bearer eyJhbGciOiJIUzI.1NiIsInR5cCI6IkpXVCJ9.eyJz
+     * Content-Type: application/json
+     *</blockquote></pre>
+     * @param request El request de la petición
+     * @return El token si existen, de lo contrario retorna {@code null}
+     */
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String headerAuthorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        //Verficamos que no sea nulo y que empiece con Bearer
+        if(StringUtils.hasText(headerAuthorization) && headerAuthorization.startsWith("Bearer ")) {
+            String token = headerAuthorization.substring(7);
+            return token;
+        }
+        return null;
     }
 }
--- user-service/src/main/java/com/hotel/booking/services/AuthService.java
+++ user-service/src/main/java/com/hotel/booking/services/AuthService.java
@@ -8,6 +8,9 @@ import com.hotel.booking.models.dtos.response.AuthResponse;
 import com.hotel.booking.models.entities.User;
 import com.hotel.booking.models.enums.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +22,23 @@ public class AuthService {
     private final JwtService jwtService;
 
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public User login(LoginRequest loginRequest) {
+    public AuthResponse login(LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
         User user = userService.findUserByEmail(loginRequest.getEmail());
+        String token = jwtService.generateToken(user);
 
-        if(!user.getPassword().equals(loginRequest.getPassword())) {
-            throw new UserNotFoundException("User Not Found");
-        }
-        return user;
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(UserRequest userRequest) {
