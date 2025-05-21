f269b46f55f44f7865529742822b9463d836ba57
JorgeGutierrez11 <jorge1706gutierrez@gmail.com>
2025-05-21 11:27:20 -0500

    Back-end update 2

--- FRONT/frontend-web-project/src/api/services/auth.service.ts
+++ FRONT/frontend-web-project/src/api/services/auth.service.ts
@@ -2,24 +2,13 @@ import { getHttpClient } from "./axios.service";
 
 export const loginUser = async (email: string, password: string) => {
     const http = getHttpClient();
-    
-    try {
-        const response = await http.post("/auth/login", { email, password });
 
-        const { token } = response.data;
+    console.log(`data: ${email} - ${password}`)
+    const response = await http.post("/auth/login", { email, password });
+    console.log(`esto esta mal? ${response}`)
+    const { token } = response.data;
 
-        if (!token) {
-            throw new Error("Token no recibido desde el servidor.");
-        }
+    localStorage.setItem("token", token);
 
-        localStorage.setItem("token", token);
-
-        return response.data;
-    } catch (error: any) {
-        if (error.response && error.response.status === 401) {
-            throw new Error("Credenciales inválidas. Verifica tu correo y contraseña.");
-        }
-
-        throw new Error("Ocurrió un error al iniciar sesión. Intenta nuevamente.");
-    }
+    return response.data;
 };
